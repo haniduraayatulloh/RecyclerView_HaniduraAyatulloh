7eed6458796b17af746c798fc350f984a5efa738
haniduraayatulloh <haniduraayatulloh8@gmail.com>
2025-11-15 11:20:45 +0700

    push

---
+++ app/src/main/java/com/example/myrecyclerview/ListHeroAdapter.kt
@@ -0,0 +1,50 @@
+package com.example.myrecyclerview
+
+import android.view.LayoutInflater // WAJIB
+import android.view.View // WAJIB
+import android.view.ViewGroup // WAJIB
+import android.widget.ImageView // WAJIB
+import android.widget.TextView // WAJIB
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import java.util.ArrayList // WAJIB (jika belum ada)
+
+class ListHeroAdapter(private val listHero: ArrayList<Hero>) : RecyclerView.Adapter<ListHeroAdapter.ListViewHolder>() {
+
+    private lateinit var onItemClickCallback: OnItemClickCallback // Untuk click listener
+
+    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
+        this.onItemClickCallback = onItemClickCallback
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_row_hero, parent, false)
+        return ListViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        val (name, description, photo) = listHero[position]
+        Glide.with(holder.itemView.context)
+            .load(photo) // URL Gambar
+            .into(holder.imgPhoto)
+        holder.tvName.text = name
+        holder.tvDescription.text = description
+
+        // Implementasi klik item
+        holder.itemView.setOnClickListener {
+            onItemClickCallback.onItemClicked(listHero[holder.adapterPosition])
+        }
+    }
+
+    override fun getItemCount(): Int = listHero.size
+
+    class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imgPhoto: ImageView = itemView.findViewById(R.id.img_item_photo)
+        val tvName: TextView = itemView.findViewById(R.id.tv_item_name)
+        val tvDescription: TextView = itemView.findViewById(R.id.tv_item_description)
+    }
+
+    interface OnItemClickCallback {
+        fun onItemClicked(data: Hero)
+    }
+}
